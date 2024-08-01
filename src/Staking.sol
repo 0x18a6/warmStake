245a721234dd757dc4449751de3c24c7c351673f
0x18a6 <huffcbytecode@gmail.com>
2024-08-01 14:31:57 +0100

    add wormhole integration

--- src/Staking.sol
+++ src/Staking.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.25;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 
+import {IWormholeRelayer} from "wormhole-solidity-sdk/interfaces/IWormholeRelayer.sol";
+import {IWormholeReceiver} from "wormhole-solidity-sdk/interfaces/IWormholeReceiver.sol";
+
 import {CommonEventsAndErrors} from "./CommonEventsAndErrors.sol";
 
 /**
@@ -11,10 +14,12 @@ import {CommonEventsAndErrors} from "./CommonEventsAndErrors.sol";
  * @notice Stakers deposit i_stakingToken and claim rewards in i_rewardToken
  */
 contract Staking is Ownable(msg.sender) {
+    IWormholeRelayer public immutable wormholeRelayer;
     IERC20 public immutable i_stakingToken;
     IERC20 public immutable i_rewardToken;
 
     uint256 constant MIN_REWARD_DURAION = 7 days;
+    uint256 constant GAS_LIMIT = 50_000;
 
     uint256 public s_totalSupply;
     uint256 public s_duration;
@@ -26,6 +31,7 @@ contract Staking is Ownable(msg.sender) {
     mapping(address => uint256) public s_balanceOf;
     mapping(address user => uint256 rewards) public s_userRewardPerTokenPaid;
     mapping(address user => uint256 rewards) public s_rewardsToBeClaimed;
+    mapping(address => uint256) public s_stakeTimestamp;
 
     event RewardDurationSet(uint256 duration);
     event RewardAmountNotified(uint256 amount);
@@ -43,10 +49,10 @@ contract Staking is Ownable(msg.sender) {
      */
     function setRewardsDuration(uint256 _duration) external onlyOwner {
         if (_duration < MIN_REWARD_DURAION) {
-            revert CommonEventsAndErrors.InvalidOperation();
+            revert CommonEventsAndErrors.InvalidParam();
         }
         if (block.timestamp < s_finishAt) {
-            revert CommonEventsAndErrors.InvalidParam();
+            revert CommonEventsAndErrors.InvalidOperation();
         }
         s_duration = _duration;
         emit CommonEventsAndErrors.RewardDurationSet(_duration);
@@ -88,6 +94,7 @@ contract Staking is Ownable(msg.sender) {
         i_stakingToken.transferFrom(msg.sender, address(this), _amount);
         s_balanceOf[msg.sender] += _amount;
         s_totalSupply += _amount;
+        s_stakeTimestamp[msg.sender] = block.timestamp; // Record the timestamp
         emit CommonEventsAndErrors.Staked(msg.sender, _amount);
     }
 
@@ -105,6 +112,61 @@ contract Staking is Ownable(msg.sender) {
         emit CommonEventsAndErrors.Withdrawn(msg.sender, _amount);
     }
 
+    /**
+     * @notice Allows a user to withdraw a certain amount of their staked tokens from a chain to a target chain
+     * @param _amount The amount of staked tokens to withdraw
+     */
+    function withdrawCrossChain(uint16 _targetChain, address _targetAddress, uint256 _amount)
+        external
+        payable
+        updateReward(msg.sender)
+    {
+        bytes memory payload = abi.encode(_amount, msg.sender);
+        uint256 cost = quoteCrossChainGreeting(_targetChain);
+        if (msg.value != cost) {
+            revert CommonEventsAndErrors.InvalidParam();
+        }
+        wormholeRelayer.sendPayloadToEvm{value: cost}(
+            _targetChain,
+            _targetAddress,
+            payload,
+            0, // no receiver value needed
+            GAS_LIMIT
+        );
+    }
+
+    /**
+     * @notice Allows a user to receive a certain amount of their staked tokens on the target chain
+     * @param payload The amount of staked tokens to withdraw
+     */
+    function receiveWithdrawCrossChain(
+        bytes memory payload,
+        bytes[] memory, // additionalVaas
+        bytes32, // address that called 'sendPayloadToEvm' (Staking contract address)
+        uint16,
+        bytes32 // unique identifier of delivery
+    ) public payable {
+        if (msg.sender != address(wormholeRelayer)) {
+            revert();
+        }
+
+        // Parse the payload and do the corresponding actions!
+        (uint256 amount, address sender) = abi.decode(payload, (uint256, address));
+        s_balanceOf[sender] -= amount;
+        s_totalSupply -= amount;
+        i_stakingToken.transfer(sender, amount);
+        emit CommonEventsAndErrors.Withdrawn(msg.sender, amount);
+    }
+
+    /**
+     * @notice Returns the cost (in wei) of a greeting
+     */
+    function quoteCrossChainGreeting(uint16 targetChain) public view returns (uint256 cost) {
+        // Cost of requesting a message to be sent to
+        // chain 'targetChain' with a gasLimit of 'GAS_LIMIT'
+        (cost,) = wormholeRelayer.quoteEVMDeliveryPrice(targetChain, 0, GAS_LIMIT);
+    }
+
     function _rewardPerToken() internal view returns (uint256) {
         if (s_totalSupply == 0) {
             return s_rewardPerTokenStored;
@@ -119,8 +181,10 @@ contract Staking is Ownable(msg.sender) {
     }
 
     function _earned(address _account) public view returns (uint256) {
+        uint256 stakingDuration = block.timestamp - s_stakeTimestamp[_account];
+        uint256 rewardMultiplier = 1 + stakingDuration / (30 days); // Increase reward by 1% for each month staked
         return ((s_balanceOf[_account] * (_rewardPerToken() - s_userRewardPerTokenPaid[_account])) / 1e18)
-            + s_rewardsToBeClaimed[_account];
+            * rewardMultiplier + s_rewardsToBeClaimed[_account];
     }
 
     modifier updateReward(address _account) {
