df00e2837b44376bf8dfc0e5d018907ac877eea8
SteMak <chestedos@gmail.com>
2024-08-02 11:52:47 +0200

    improve test

--- test/Staking.t.sol
+++ test/Staking.t.sol
@@ -1,19 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.25;
 
-import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import {ERC20Mint} from "../src/ERC20Mint.sol";
 import {Test} from "forge-std/Test.sol";
 import {Staking} from "../src/Staking.sol";
 import {WormholeRelayerTest} from "wormhole-solidity-sdk/testing/WormholeRelayerTest.sol";
 
 contract StakingTest is Test {
     Staking staking;
-    IERC20 stakingToken;
-    IERC20 rewardToken;
+    ERC20Mint stakingToken;
+    ERC20Mint rewardToken;
     address notOwner = makeAddr("notOwner");
     address user = makeAddr("user");
 
     function setUp() public {
+        stakingToken = new ERC20Mint("staking", "ST");
+        rewardToken = new ERC20Mint("reward", "RT");
+
         staking = new Staking(address(stakingToken), address(rewardToken));
     }
 
@@ -33,8 +36,40 @@ contract StakingTest is Test {
 
     function test_stake() external {
         ///@notice stake 0 not allowed
-        vm.prank(user);
+        vm.startPrank(user);
         vm.expectRevert();
         staking.stake(0);
+
+        uint256 amount = 250 * 10 ** 18;
+        stakingToken.mint(amount);
+        stakingToken.approve(address(staking), amount);
+        staking.stake(amount);
+
+        assertEq(stakingToken.balanceOf(address(staking)), amount);
+        assertEq(staking.s_balanceOf(user), amount);
+        assertEq(staking.s_balanceOf(address(staking)), 0);
+
+        ///@notice withdraw 0 not allowed
+        vm.expectRevert();
+        staking.withdraw(0);
+
+
+        stakingToken.mint(1);
+        stakingToken.transfer(address(staking), 1);
+        ///@notice withdraw over balance not allowed
+        vm.expectRevert();
+        staking.withdraw(amount + 1);
+
+        staking.withdraw(amount / 5);
+
+        assertEq(stakingToken.balanceOf(user), amount / 5);
+        assertEq(staking.s_balanceOf(user), 4 * amount / 5);
+        assertEq(staking.s_balanceOf(address(staking)), 0);
+
+        staking.withdraw(4 * amount / 5);
+
+        assertEq(stakingToken.balanceOf(user), amount);
+        assertEq(staking.s_balanceOf(user), 0);
+        assertEq(staking.s_balanceOf(address(staking)), 0);
     }
 }
